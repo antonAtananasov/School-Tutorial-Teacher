0b628a074fed87a5ce1002229ff9a2eba602d980
MEGA <MEGA@gmail.com>
2025-02-17 12:19:11 +0200

    add n

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int n = 1;
+        Print(n);
+    }
+
+    static void Print(int n)
+    {
+        Console.WriteLine($"{n++} {Console.ReadLine()}");
+    }
+}
