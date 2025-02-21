57d7d33812a0a64d948e6ef5fb7e2dcefe81a1dd
Tonziss <antonatanasov2002@gmail.com>
2025-02-21 12:19:52 +0200

    add babylon sqrt to class c

--- Class_C.cs
+++ Class_C.cs
@@ -12,5 +12,18 @@ namespace School_Tutorial_Teacher
         {
             return Math.Sqrt(a);
         }
+        //programist 2 pravyat CubeRoot
+        //...
+
+        //Programist 1 pravyat Babylon
+        public double BabylonSqrt(double number)
+        {
+            double guess = number / 2;
+            while (Math.Abs(guess * guess - number) < 0.01) {
+                double other = number / guess;
+                guess = (guess+other)/2;
+            }
+            return guess;
+        }
     }
 }
