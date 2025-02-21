f82d117a0b73b747bec20107fedd9552427e9b89
MEGA <MEGA@gmail.com>
2025-02-21 11:39:58 +0200

    add Sqrt class c

---
+++ Class_C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Tutorial_Teacher
+{
+    internal class Class_C
+    {
+        public double Sqrt(double a)
+        {
+            return Math.Sqrt(a);
+        }
+    }
+}
