86bca3a6bcef457640226007d6d2ff97925dbbaf
MEGA <MEGA@gmail.com>
2025-03-07 12:10:37 +0200

    removed class c from main

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-static void Main(string[] args)
-{
-    Console.WriteLine("Hello, World!");
-    Console.ReadKey();
-}
